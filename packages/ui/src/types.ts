71cf16aa8e128ac845421d16b38ecc7ab480fb35
rlatlswo135 <rlatlswo13@gmail.com>
2026-02-25 16:46:34 +0900

    feat(funnel.tsx): complete funnel component

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -2,6 +2,14 @@ import type { ReactElement, ReactNode } from "react";
 
 export type ClickableElement = ReactElement<{ onClick: () => void }>;
 
-export type RenderChildrenProps<T extends Record<string, unknown>> = {
-  children: ReactElement | ((props: T) => ReactNode);
+type Dict = Record<string, unknown>;
+type Factory<T, U> = (props: T) => U;
+
+export type FnChildren<T extends Dict> = {
+  children: ReactNode | Factory<T, ReactNode>;
+};
+
+// TODO: docs에 ReactElement -> 단일 children이어야 하는걸 명시
+export type ElementFnChildren<T extends Dict> = {
+  children: ReactElement | Factory<T, ReactNode>;
 };
