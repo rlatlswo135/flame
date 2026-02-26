90024a8d35efde66dd08ab9e9d90f6938800f2f1
rlatlswo135 <rlatlswo13@gmail.com>
2026-02-26 10:15:04 +0900

    feat(dialog.tsx): keepMounted feature

--- packages/ui/src/components/dialog/context.ts
+++ packages/ui/src/components/dialog/context.ts
@@ -1,10 +1,18 @@
-import { createContext, type RefObject } from "react";
+import {
+  createContext,
+  type Dispatch,
+  type RefObject,
+  type SetStateAction,
+} from "react";
 
-export type DialogContextValue = {
+type DialogContextValue = {
   dialog: RefObject<HTMLDialogElement | null>;
   open: () => void;
   close: () => void;
   closeOutside?: boolean;
+  keepMounted?: boolean;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 export const DialogContext = createContext<DialogContextValue | null>(null);
--- packages/ui/src/components/dialog/index.tsx
+++ packages/ui/src/components/dialog/index.tsx
@@ -1,33 +1,54 @@
 import {
   cloneElement,
-  useMemo,
   useRef,
+  useState,
   type DialogHTMLAttributes,
   type PropsWithChildren,
 } from "react";
-import { DialogContext, type DialogContextValue } from "./context";
-import type { ClickableElement, RenderChildrenProps } from "@/src/types";
+import { DialogContext } from "./context";
+import type { ClickableElement, ElementFnChildren } from "@/src/types";
 import { useCtx } from "@/src/hooks/use-ctx";
 
-type DialogProps = PropsWithChildren<{ closeOutside?: boolean }>;
+type DialogProps = PropsWithChildren<{
+  closeOutside?: boolean;
+  keepMounted?: boolean;
+}>;
 
-const Dialog = ({ children, closeOutside = false }: DialogProps) => {
-  const ref = useRef<HTMLDialogElement>(null);
+const Dialog = ({
+  children,
+  closeOutside = false,
+  keepMounted = false,
+}: DialogProps) => {
+  const dialog = useRef<HTMLDialogElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const contextValue: DialogContextValue = useMemo(
-    () => ({
-      dialog: ref,
-      open: () => ref.current?.showModal(),
-      close: () => ref.current?.close(),
-      closeOutside,
-    }),
-    [closeOutside],
+  const open = () => {
+    dialog.current?.showModal();
+    if (!keepMounted) setIsOpen(true);
+  };
+
+  const close = () => {
+    dialog.current?.close();
+  };
+
+  return (
+    <DialogContext
+      value={{
+        open,
+        dialog,
+        close,
+        isOpen,
+        setIsOpen,
+        closeOutside,
+        keepMounted,
+      }}
+    >
+      {children}
+    </DialogContext>
   );
-
-  return <DialogContext value={contextValue}>{children}</DialogContext>;
 };
 
-const Trigger = ({ children }: RenderChildrenProps<{ open: () => void }>) => {
+const Trigger = ({ children }: ElementFnChildren<{ open: () => void }>) => {
   const { open } = useCtx(DialogContext);
 
   if (typeof children === "function") return children({ open });
@@ -35,7 +56,7 @@ const Trigger = ({ children }: RenderChildrenProps<{ open: () => void }>) => {
   return cloneElement(children as ClickableElement, { onClick: open });
 };
 
-const Closer = ({ children }: RenderChildrenProps<{ close: () => void }>) => {
+const Closer = ({ children }: ElementFnChildren<{ close: () => void }>) => {
   const { close } = useCtx(DialogContext);
 
   if (typeof children === "function") return children({ close });
@@ -47,11 +68,23 @@ const Content = ({
   children,
   ...props
 }: PropsWithChildren<DialogHTMLAttributes<HTMLDialogElement>>) => {
-  const { dialog, close, closeOutside } = useCtx(DialogContext);
+  const ctx = useCtx(DialogContext);
+
+  const onClose = (e: React.SyntheticEvent<HTMLDialogElement>) => {
+    if (!ctx.keepMounted) ctx.setIsOpen(false);
+    props?.onClose?.(e);
+  };
 
   return (
-    <dialog ref={dialog} onClick={closeOutside ? close : undefined} {...props}>
-      <section onClick={(e) => e.stopPropagation()}>{children}</section>
+    <dialog
+      ref={ctx.dialog}
+      onClick={ctx.closeOutside ? ctx.close : undefined}
+      {...props}
+      onClose={onClose}
+    >
+      <section onClick={(e) => e.stopPropagation()}>
+        {(ctx.keepMounted || ctx.isOpen) && children}
+      </section>
     </dialog>
   );
 };
