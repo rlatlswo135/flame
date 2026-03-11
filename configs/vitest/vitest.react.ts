91c69a854f2a28299b87769483afef91140301c5
rlatlswo135 <rlatlswo13@gmail.com>
2026-03-11 09:30:11 +0900

    chore: vitest ing

--- configs/vitest/vitest.react.ts
+++ configs/vitest/vitest.react.ts
@@ -1,11 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const vitestReactConfig = defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'happy-dom',
-    globals: true, // no import assertion just add global
-    setupFiles: ['vitest.react.setup.ts'],
+    setupFiles: [path.resolve(__dirname, 'vitest.react.setup.ts')],
   },
 });
