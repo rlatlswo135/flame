10154fda5bf4dfa7abc8e6d12f483759fa5e16fb
rlatlswo135 <rlatlswo13@gmail.com>
2026-03-12 17:26:09 +0900

    DUCK DUCK 🦆

--- packages/ui/src/components/popover/index.tsx
+++ packages/ui/src/components/popover/index.tsx
@@ -1,3 +1,58 @@
+import {
+	autoUpdate,
+	FloatingFocusManager,
+	flip,
+	offset,
+	shift,
+	useClick,
+	useDismiss,
+	useFloating,
+	useInteractions,
+	useRole,
+} from "@floating-ui/react";
+import { useState } from "react";
+
+export type PopoverProps = {};
+
 export const Popover = () => {
-	return <div>popover</div>;
+	const [isOpen, setIsOpen] = useState(false);
+
+	const { refs, floatingStyles, context } = useFloating({
+		open: isOpen,
+		onOpenChange: setIsOpen,
+		middleware: [offset(10), flip(), shift()],
+		whileElementsMounted: autoUpdate,
+	});
+
+	const click = useClick(context);
+	const dismiss = useDismiss(context);
+	const role = useRole(context);
+
+	// Merge all the interactions into prop getters
+	const { getReferenceProps, getFloatingProps } = useInteractions([
+		click,
+		dismiss,
+		role,
+	]);
+
+	return (
+		<div>
+			<h1>Floating UI Popover</h1>
+			<button ref={refs.setReference} {...getReferenceProps()}>
+				Reference element
+			</button>
+			{isOpen && (
+				<FloatingFocusManager context={context} modal={false}>
+					<div
+						ref={refs.setFloating}
+						style={floatingStyles}
+						{...getFloatingProps()}
+						className="Popover"
+					>
+						Popover element
+					</div>
+				</FloatingFocusManager>
+			)}
+		</div>
+	);
 };
