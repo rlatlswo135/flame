fbc9df82b2dfc014f99d2cb3e953424c94c1cf40
rlatlswo135 <rlatlswo13@gmail.com>
2026-04-21 14:26:44 +0900

    DUCK DUCK 🦆

--- packages/ui/src/hooks/use-key-down.ts
+++ packages/ui/src/hooks/use-key-down.ts
@@ -1,1 +1,12 @@
+type UseKeydownProps = {
+	enabled?: boolean;
+	key:
+		| "Enter"
+		| "ArrowDown"
+		| "ArrowUp"
+		| "ArrowLeft"
+		| "ArrowRight"
+		| "Escape";
+};
+
 export const useKeyDown = () => {};
