0eeee9d07481709aa8f3ff4e9971caf15d525e51
rlatlswo135 <rlatlswo13@gmail.com>
2026-04-26 22:29:01 +0900

    DUCK DUCK 🦆

--- packages/ui/src/hooks/use-key-down.ts
+++ packages/ui/src/hooks/use-key-down.ts
@@ -1,12 +1,22 @@
+import { useEffect, useEffectEvent } from "react";
+
+type Key =
+  | "Enter"
+  | "ArrowDown"
+  | "ArrowUp"
+  | "ArrowLeft"
+  | "ArrowRight"
+  | "Escape";
+
 type UseKeydownProps = {
-	enabled?: boolean;
-	key:
-		| "Enter"
-		| "ArrowDown"
-		| "ArrowUp"
-		| "ArrowLeft"
-		| "ArrowRight"
-		| "Escape";
+  key: Key;
+  handler: () => void;
+  enabled?: boolean;
 };
 
-export const useKeyDown = () => {};
+export const useKeyDown = ({ key, handler, enabled }: UseKeydownProps) => {
+  // TODO: useEffectEvent 활용
+  const event = useEffectEvent(handler);
+
+  useEffect(() => {}, []);
+};
